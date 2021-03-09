1d1909b8f3ca9b0358c6b7670338a041200286af
jaros <jaros@DESKTOP-99R6M09>
2021-03-09 17:18:31 -0300

    solucionando errores heroku

--- index.js
+++ index.js
@@ -1,64 +1,47 @@
-import express from 'express';
-import router from './routes/index.js';
-import db from './config/db.js';
-import dotenv from 'dotenv';
+import express from 'express'
+import router from './routes/index.js'
+import db from './config/db.js'
+
+import dotenv from "dotenv";
+const app= express();
 
 
-dotenv.config({path:"variables.env"});
+dotenv.config({ path: "./variables.env" });
 
-//Conectar la base de datos
+// conectar la base de datos
 db.authenticate()
-    .then( () => console.log('Base de datos Conectada'))
-    .catch( error => console.log(error));
+    .then(()=> console.log('Base de datos conectada'))
+    .catch(error => console.log(error));
 
-        
-//Configurar express
-const app = express();
 
-//Habilitar PUG
-app.set('view engine', 'pug');
+//Definir puerto
+const port=process.env.PORT || 4001;
 
-//Añadir las vistas
-app.set('views', path.join(__dirname, './views'));
+/** Puerto y host para la app */
+const host = process.env.HOST || '0.0.0.0';
 
-//Cargar una carpeta estatica llamada public
+//Habilitar Pug
+app.set('view engine','pug');
+
+//obtener el año actual
+app.use ((req,res,next)=>{
+    const year = new Date();
+    res.locals.actualYear=year.getFullYear();
+    res.locals.nombresitio="Agencia de viajes";
+    next();
+});
+// Agregar body parser para leer los datos del formulario 
+app.use(express.urlencoded({extended:true}));
+
+//Definir la carpeta publica 
 app.use(express.static('public'));
 
-//Validar si estamos en desarrollo o produccion
-const config = db[app.get('env')];
-
-//Obtener año actual
-app.use((req,res, next) => {
-    const year = new Date()
-    res.locals.actualYear = year.getFullYear();
-    res.locals.ruta = req.path;
-    return next();
-});
-
-
-//Body Parser para leer datos del formulario
-app.use(express.urlencoded({extended: true}));
-
-//Cargar las Rutas
-app.use('/', router);
-
-//Puerto y host para la app
-const host = process.env.HOST || '0.0.0.0';
-const port = process.env.PORT || 3000;
-
+//Agregar router
+app.use('/',router);
 
 app.listen(port, host, () => {
-    console.log('El servidor esta funcionando')
-});
-  
-
-
-
-//Creamos la variable para el sitio web
-//app.locals.titulo = config.nombreSitio;
-
-
-
+    console.log('El servidor esta funcionando en el puerto ${port} xd')
+})
 
 
 
