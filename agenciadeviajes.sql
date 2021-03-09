a3365724efdba5d80eb04275c191ebcf76d08b4d
jaros <jaros@DESKTOP-99R6M09>
2021-03-09 17:53:42 -0300

    problema sql solucionado

--- agenciadeviajes.sql
+++ agenciadeviajes.sql
@@ -28,7 +28,7 @@ CREATE TABLE `testimoniales` (
   `correo` varchar(60) DEFAULT NULL,
   `mensaje` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
